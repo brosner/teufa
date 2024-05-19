5b40d06bf4bfccfcbb2714db356210c9907384d2
Brian Rosner <brian@brosner.com>
2024-05-19 17:07:41 -0600

    config

--- teufa/app.py
+++ teufa/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 
+from .config import Config
 from .ext import db
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config())
+
     db.init_app(app)
+
     return app
